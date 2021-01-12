282c316773303202b3038c1033db774479b951a4
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2021-01-11 17:28:20 -0800

    feat(main-app): add loading animations
    added loading animation when making api request and also added error message if there is any

---
+++ src/components/Loader.tsx
@@ -0,0 +1,12 @@
+import BeatLoader from "react-spinners/BeatLoader";
+
+
+const Loader = () => {
+  return (
+    <div className="sweet-loading">
+      <BeatLoader color={"#333"} size={20} />
+    </div>
+  );
+}
+
+export default Loader;
